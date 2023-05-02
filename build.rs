0dfefee81ef972abd4a3fea4e54d28d5e9c361c5
Jonny Burger <jonathanburger11@gmail.com>
2023-05-02 13:54:52 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -368,7 +368,7 @@ fn get_zip_name() -> String {
             "x86_64-apple-darwin" => "macos-x64.gz".to_string(),
             "aarch64-apple-darwin" => "macos-arm.gz".to_string(),
             "x86_64-pc-windows-gnu" => "windows.gz".to_string(),
-            &_ => panic!("Unsupported target"),
+            tar => panic!("Unsupported target {}", tar),
         },
         Err(_) => panic!("TARGET environment variable not set"),
     }
